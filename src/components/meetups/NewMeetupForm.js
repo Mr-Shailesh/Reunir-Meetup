dfad70b4504b2e43478dd421cd3380fd75985ae8
c100-103 <hss@narola.email>
2022-06-24 11:50:25 +0530

    Change in NewMeetupForm

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -35,7 +35,7 @@ const NewMeetupForm = (props) => {
           <input type="text" required id="title" ref={titleInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor="image">Meetup Image</label>
+          <label htmlFor="image">Meetup Image URL</label>
           <input type="url" required id="image" ref={imageInputRef} />
         </div>
         <div className={classes.control}>
